d28f56f58a8dab1af742f0652f4264daa4816b76
Ankush Mishra <ankushmishra0939@gmail.com>
2021-08-08 10:52:59 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { ChatEngine } from 'react-chat-engine';
 
 import ChatFeed from './component/ChatFeed';
+import LoginForm from './component/LoginForm';
 
 import './App.css';
 
 const App = () => {
+    if(!localStorage.getItem('username')) return <LoginForm />
     return(
         <ChatEngine 
             heignt="100vh"
             projectID="8913aaef-ab0c-4d32-a87b-d89400b7bbef"
-            userName="Ankush"
-            userSecret="ankush123"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
             renderChatFeed = {(chatAppProps) => <ChatFeed {...chatAppProps}/>}
         />
     )
